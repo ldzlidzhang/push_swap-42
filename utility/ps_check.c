f4d74f0ebf34cf9fabc0a59f25d3642150e41d35
Lidan Zhang <116870623+ldzlidzhang@users.noreply.github.com>
2023-03-14 08:27:56 +0200

    error control final

--- utility/ps_check.c
+++ utility/ps_check.c
@@ -6,7 +6,7 @@
 /*   By: lidanzhang <lidanzhang@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:09:16 by lidanzhang        #+#    #+#             */
-/*   Updated: 2023/03/14 00:34:40 by lidanzhang       ###   ########.fr       */
+/*   Updated: 2023/03/14 08:21:11 by lidanzhang       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,15 @@ static int	ft_isnum(char *num)
 static char	**argc2(char *av)
 {
 	char	**args;
+	int		i;
 
+	i = 0;
+	while (av[i])
+	{
+		if (!ft_isdigit(av[i]) && av[i] != ' ' && av[i] != '-' && av[i] != '+')
+			ps_error("Error");
+		i++;
+	}
 	args = ft_split(av, ' ');
 	return (args);
 }
@@ -58,7 +66,10 @@ void	check_args(int argc, char **argv)
 
 	i = 1;
 	if (argc == 2)
+	{
 		args = argc2(argv[1]);
+		i = 0;
+	}
 	else
 		args = argv;
 	while (args[i])
@@ -75,31 +86,3 @@ void	check_args(int argc, char **argv)
 	if (argc == 2)
 		ps_free(args);
 }
-
-// void	check_args(int argc, char **argv)
-// {
-// 	int		i;
-// 	long	tmp;
-// 	char	**args;	
-
-// 	i = 0;
-// 	args = ft_split(argv[1], ' ');
-// 	if (argc == 2 && !ft_isnum(argv[1]))
-// 		ps_error("Error");
-// 	else
-// 	{
-// 		i = 1;
-// 		args = argv;
-// 	}
-// 	while (args[i])
-// 	{
-// 		tmp = ft_atoi(args[i]);
-// 		if (!ft_isnum(args[i]))
-// 			ps_error("Error");
-// 		if (ft_contains(tmp, args, i) == 1)
-// 			ps_error("Error");
-// 		if (tmp < INT32_MIN || tmp > 2147483647)
-// 			ps_error("Error");
-// 		i++;
-// 	}
-// }
